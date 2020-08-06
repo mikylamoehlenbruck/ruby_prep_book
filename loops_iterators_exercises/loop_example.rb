07864930e716d90cac8777e216e1074f539302db
Mikyla Moehlenbruck <m.moehlenbruck11@gmail.com>
2020-08-06 12:25:15 -0700

    Loops and Iterators exercises

---
+++ loops_iterators_exercises/loop_example.rb
@@ -0,0 +1,3 @@
+loop do
+  puts "This will print until you hit Ctrl + c"
+end
---
+++ loops_iterators_exercises/next_loop.rb
@@ -0,0 +1,12 @@
+i = o
+
+loop do
+  i += 2
+  if i == 4
+    next
+  end
+  puts  i
+  if i == 10
+    break
+  end
+end
