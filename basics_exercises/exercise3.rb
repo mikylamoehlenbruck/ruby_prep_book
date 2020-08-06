656555b880d2cdf2614dd8cb02ffbbf895e2d5e9
Mikyla Moehlenbruck <m.moehlenbruck11@gmail.com>
2020-08-06 11:54:15 -0700

    Completed exercises 3-6

---
+++ basics_exercises/exercise3.rb
@@ -0,0 +1,12 @@
+movies = {
+  star_wars: 1975,
+  avatar: 2004,
+  harry_potter: 2013,
+  oceans_11: 2001,
+  pets: 2011 }
+
+puts movies[:star_wars]
+puts movies[:avatar]
+puts movies[:harry_potter]
+puts movies[:oceans_11]
+puts movies[:pets]
